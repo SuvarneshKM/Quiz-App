38a74672f5571012c7abd00cfc2376ede0cfbaea
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-02-03 10:41:08 +0530

    commit

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create_quiz.html', views.create_quiz, name="create_quiz"),
+    path('join_quiz.html', views.join_quiz, name="join_quiz"),
     ]    
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home.html', {}) 
+
+def create_quiz(request):
+    return render(request, 'create_quiz.html', {}) 
+
+def join_quiz(request):
+    return render(request, 'join_quiz.html', {})         
